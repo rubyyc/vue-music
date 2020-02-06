902be90faf83c5a172ccd55064becbf260ffe54c
yuanchang <yuanchang2018@gmail.com>
2020-02-06 11:59:40 +0800

    min player适配

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -33,11 +33,13 @@
 	import {prefixStyle} from 'common/js/dom'
 	import Loading from 'base/loading/loading'
 	import {mapActions} from 'vuex'
+	import {playlistMixin} from 'common/js/mixin'
 
 	const RESERVED_HEIGHT = 40
 	const transform = prefixStyle('transform')
 	const backdrop = prefixStyle('backdrop-filter')
 	export default {
+		mixins: [playlistMixin],
 		props: {
 			bgImage: {
 				type: String,
@@ -73,6 +75,11 @@
 			// console.log(this.imageHeight)
 		},
 		methods: {
+			handlePlaylist(playlist) {
+				const bottom = playlist.length > 0 ? '60px' : ''
+				this.$refs.list.$el.style.bottom = bottom
+				this.$refs.list.refresh()
+			},
 			random() {
 				this.randomPlay({
 					list: this.songs
